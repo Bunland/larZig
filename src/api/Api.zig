7a0b1b3912cbbc5b66bcc9a236f707740f61f80a
vitalspace <myglobalsounds@gmail.com>
2023-12-29 21:51:52 -0600

    updated

---
+++ src/api/Api.zig
@@ -0,0 +1,48 @@
+// Import standard, jsc, Maths, and Console libraries
+const std = @import("std");
+const jsc = @import("../jsc/jsc.zig");
+const Maths = @import("../apis//maths/maths.zig").Maths;
+const Console = @import("../apis/console/console.zig").Console;
+const Fs = @import("../apis/fs/fs.zig").Fs;
+const constants = @import("../constants/constants.zig");
+
+// Define the Apis structure
+pub const Apis = struct {
+    const this = @This();
+
+    // Initialize the APIs
+    pub fn init(ctx: jsc.JSContextRef, globalObject: jsc.JSObjectRef) !void {
+        try this.LarApis(ctx, globalObject);
+    }
+
+    // Define the LarApis function
+    fn LarApis(ctx: jsc.JSContextRef, globalObject: jsc.JSObjectRef) !void {
+        _ = this;
+        const larStr = jsc.JSStringCreateWithUTF8CString(constants.lar);
+        defer jsc.JSStringRelease(larStr);
+
+        const larGlobalObject = jsc.JSObjectMake(ctx, null, null);
+        jsc.JSObjectSetProperty(ctx, globalObject, larStr, larGlobalObject, jsc.kJSClassAttributeNone, null);
+
+        // Initialize the Console
+        try Console.init(ctx, globalObject);
+
+        // Create a custom function
+        try this.larCustomFunction(ctx, globalObject, "Add", Maths.Add);
+
+        // Fs
+        try this.larCustomFunction(ctx, larGlobalObject, constants.writeFile, Fs.writeFile);
+        try this.larCustomFunction(ctx, larGlobalObject, constants.readFile, Fs.readFile);
+        try this.larCustomFunction(ctx, larGlobalObject, constants.removeFile, Fs.removeFile);
+        try this.larCustomFunction(ctx, larGlobalObject, constants.existsFile, Fs.existsFile);
+    }
+
+    // Define a custom function
+    fn larCustomFunction(ctx: jsc.JSContextRef, globalObject: jsc.JSObjectRef, functionName: []const u8, functionCallBack: jsc.JSObjectCallAsFunctionCallback) !void {
+        const functionString = jsc.JSStringCreateWithUTF8CString(functionName.ptr);
+        defer jsc.JSStringRelease(functionString);
+
+        const functionObject = jsc.JSObjectMakeFunctionWithCallback(ctx, functionString, functionCallBack);
+        jsc.JSObjectSetProperty(ctx, globalObject, functionString, functionObject, jsc.kJSPropertyAttributeNone, null);
+    }
+};
--- src/apis/maths/maths.zig
+++ src/apis/maths/maths.zig
@@ -1,38 +1,22 @@
 const std = @import("std");
 const jsc = @import("../../jsc/jsc.zig");
 
-pub fn Add(context: jsc.JSContextRef, globalObject: jsc.JSObjectRef, thisObject: jsc.JSObjectRef, argumentsCount: usize, arguments: [*c]const jsc.JSValueRef, execption: [*c]jsc.JSValueRef) callconv(.C) jsc.JSValueRef {
-    _ = execption;
-    _ = thisObject;
-    _ = globalObject;
+pub const Maths = struct {
+    const this = @This();
 
-    if (argumentsCount < 2) {
-        std.debug.print("The function requires 2 aguments\n", .{});
-        return jsc.JSValueMakeUndefined(context);
+    pub fn Add(ctx: jsc.JSContextRef, globalObject: jsc.JSObjectRef, thisObject: jsc.JSObjectRef, argumentsCount: usize, arguments: [*c]const jsc.JSValueRef, execption: [*c]jsc.JSValueRef) callconv(.C) jsc.JSValueRef {
+        _ = globalObject;
+        _ = thisObject;
+        _ = execption;
+
+        if (argumentsCount < 2) {
+            std.debug.print("The function requires 2 arguments\n", .{});
+            return jsc.JSValueMakeUndefined(ctx);
+        }
+
+        var numa = jsc.JSValueToNumber(ctx, arguments[0], null);
+        var numb = jsc.JSValueToNumber(ctx, arguments[1], null);
+        var result = numa + numb;
+        return jsc.JSValueMakeNumber(ctx, result);
     }
-
-    var numa = jsc.JSValueToNumber(context, arguments[0], null);
-    var numb = jsc.JSValueToNumber(context, arguments[1], null);
-
-    var add = numa + numb;
-    // std.debug.print("{d}\n", .{add});
-    return jsc.JSValueMakeNumber(context, add);
-}
-
-pub fn Mult(context: jsc.JSContextRef, globalObject: jsc.JSObjectRef, thisObject: jsc.JSObjectRef, argumentsCount: usize, arguments: [*c]const jsc.JSValueRef, execption: [*c]jsc.JSValueRef) callconv(.C) jsc.JSValueRef {
-    _ = execption;
-    _ = thisObject;
-    _ = globalObject;
-
-    if (argumentsCount < 2) {
-        std.debug.print("The function requires 2 aguments\n", .{});
-        return jsc.JSValueMakeUndefined(context);
-    }
-
-    var numa = jsc.JSValueToNumber(context, arguments[0], null);
-    var numb = jsc.JSValueToNumber(context, arguments[1], null);
-
-    var add = numa * numb;
-    // std.debug.print("{d}\n", .{add});
-    return jsc.JSValueMakeNumber(context, add);
-}
+};

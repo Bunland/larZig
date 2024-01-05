b2a7c5b21e2ebd78e0de63906b6273470c5dfd26
vitalspace <myglobalsounds@gmail.com>
2024-01-04 20:27:27 -0600

    added console.assert method

--- zig-out/bin/index.js
+++ zig-out/bin/index.js
@@ -3,6 +3,16 @@ console.log(lar.readFile("alv.txt"));
 console.log(lar.existsFile("alv.txt"));
 console.log(lar.removeFile("alv.txt"));
 console.clear();
+console.assert(5 == 5, "true");
+const myArray = [1, 2, 3];
+const element = 4;
+
+console.assert(myArray.includes(element), `The element ${element} is not present in the array`);
+
+
+
+
+
 
 // const obj = {
   // id: 1,

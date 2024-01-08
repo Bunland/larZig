23a8b9138c0cfea7cf24b7e7c684677fa5dd2788
vitalspace <myglobalsounds@gmail.com>
2024-01-07 20:08:15 -0600

    updated

--- zig-out/bin/index.js
+++ zig-out/bin/index.js
@@ -1,15 +1,79 @@
-console.log(lar.writeFile("alv.txt", "lucas"));
-console.log(lar.readFile("alv.txt"));
-console.log(lar.existsFile("alv.txt"));
-console.log(lar.removeFile("alv.txt"));
-console.clear();
-console.assert(5 == 5, "true");
-const myArray = [1, 2, 3];
-const element = 4;
+// console.log(lar.writeFile("alv.txt", "lucas"));
+// console.log(lar.readFile("alv.txt"));
+// console.log(lar.existsFile("alv.txt"));
+// console.log(lar.removeFile("alv.txt"));
+// console.clear();
+// console.assert(5 == 5, "true");
+// const myArray = [1, 2, 3];
+// const element = 4;
+// 
+// console.assert(myArray.includes(element), `The element ${element} is not present in the array`);
 
-console.assert(myArray.includes(element), `The element ${element} is not present in the array`);
+
+// console.count()
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Lucas")
+// console.count("Lucas")
+// console.count("Sam")
+// console.count("Lucas")
+// console.count("Lucas")
+// console.count("Lucas")
+// console.count("Ejemplo")
+// console.count("Sam")
+// console.count("Ejemplo")
+// console.count("")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count()
+// console.count()
+// console.count()
+// console.count()
+// console.count()
+// console.count("")
+// console.count("")
+// console.count("")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count()
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
+// console.count("")
+// console.count("")
+// console.count("")
+// console.count("")
+// console.count("")
+// console.count("Ejemplo")
+// console.count("Ejemplo")
 
 
+
+// console.count("Ejemplo")
+
+// console.count("Ejemplo")
+// console.count()
+// console.count("Ejemplo1")
+// console.count()
+// console.count("Ejemplo")
+// console.count()
+// console.count()
+// console.count("Ejemplo1")
+// console.count()
+// console.count()
 
 
 
@@ -22,4 +86,9 @@ console.assert(myArray.includes(element), `The element ${element} is not present
 // console.log( "SOY LAR", lar.Server(JSON.stringify(obj)));
 
 
+for (let index = 0; index < 1000000; index++) {
+  // const element = array[index];
 
+  console.count("Hello",index)
+  
+}
